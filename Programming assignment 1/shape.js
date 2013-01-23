d882819e092cae6ac512a68eb5e65e8eb093cfbe
Finnbjörn Þorvaldsson <finnbjorn@finnbjorn.com>
2013-01-23 15:43:26 +0000

    Uppfaerdi shape.js

--- Programming assignment 1/shape.js
+++ Programming assignment 1/shape.js
@@ -1,1 +1,30 @@
-console.log('Shape')
+console.log( "Shape class loading." );
+
+//var shape = new Object();
+
+function Shape( x, y ){
+	// Initialize the shape to x=0 and y=0.
+	
+	var m_x = ( x === undefined ) ? 0 : x;
+	var m_y = ( y === undefined ) ? 0 : y;
+
+	this.draw() = function(){
+		// Empty, is abstract and
+		// should be overloded in
+		// derived "classes"!
+	}
+
+	this.getX = function(){
+		return m_x;
+	}
+
+	this.getY = function(){
+		return m_y;
+	}
+}
+var shape = new Shape();
+
+console.log( "this.x = " + this.x );
+console.log( "shape.getX = " + shape.getX() );
+
+console.log( "Shape class loaded." );
