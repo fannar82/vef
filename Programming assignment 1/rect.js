f4f36504dd93f27d1c5c02f8a48ae5c855572798
Finnbjörn Þorvaldsson <finnbjorn@finnbjorn.com>
2013-01-23 16:36:55 +0000

    Eftir timann 23.01

--- Programming assignment 1/rect.js
+++ Programming assignment 1/rect.js
@@ -1,21 +1,43 @@
-//Rectangle class
-	$(canvas).mousedown( function(e) {
-		// "offsetX/offsetY" works in Chrome and Opera
-		x = e.offsetX; //e.pageX - this.offsetLeft;
-		y = e.offsetY; //e.pageY - this.offsetTop;
-	});
+function Rectangle ( x, y ) {
 
-	$(canvas).mouseup( function(e) {
+	//Rectangle class	
+	var m_x = ( x === undefined ) ? 0 : x;
+	var m_y = ( y === undefined ) ? 0 : y;
+
+	var m_dx;
+	var m_dy;
+
+	// this.draw() = function(){
+	// 	// Empty, is abstract and
+	// 	// should be overloded in
+	// 	// derived "classes"!
+	// }
+
+	this.getX = function(){
+		return m_x;
+	}
+
+	this.getY = function(){
+		return m_y;
+	}
+		// // "offsetX/offsetY" works in Chrome and Opera
+		// x = e.offsetX; //e.pageX - this.offsetLeft;
+		// y = e.offsetY; //e.pageY - this.offsetTop;
+
+
+	function setDelta ( x, y ) {
+		console.log("bx= " + x + " by= " + y);
+		m_dx = x - this.offsetLeft - m_x ;
+		m_dy = y - this.offsetTop - m_y;
+	}
+
+	function draw () {
 		context.fillStyle = "blue";
+		context.fillRect(m_x, m_y, m_dx, m_dy);
+	}
 
-		dx = e.pageX - this.offsetLeft - x ;
-		dy = e.pageY - this.offsetTop - y;
-
-		context.fillRect(x, y, dx, dy);
-
-		console.log('x = ' + x);
-		console.log('y = ' + y);
-		console.log('dx = ' + dx);
-		console.log('dy = ' + dy);
-
-	})
+	console.log('x = ' + m_x);
+	console.log('y = ' + m_y);
+	console.log('dx = ' + m_dx);
+	console.log('dy = ' + m_dy);
+}
