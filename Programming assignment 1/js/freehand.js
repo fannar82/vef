33b1812d67f014195000d3dd2d890e5d6d165cbb
unknown <fannar82@gmail.com>
2013-01-31 00:48:51 +0000

    Buið að laga Undo og annað mikið stöff

--- Programming assignment 1/js/freehand.js
+++ Programming assignment 1/js/freehand.js
@@ -3,25 +3,22 @@ console.log( "Freehand class loading." );
 var Freehand = Shape.extend({
 	constructor: function( x, y ) {
 		this.base( x, y, color );
+		this.allX = [];
+		this.allY = [];
 		console.log( "Freehand constructor loaded." );
 	},
 
-	allX: new Array(),
-	allY: new Array(),
-	  
+  
 	
 	arrayX: function () {
 		this.allX.push( this.getEndX() );
     	this.allY.push( this.getEndY() );
 	},
 
-	// draw: function() {
- //    	this.strokeLine( this.getEndX() , this.getEndY() );
-    	
-	// },
 
 	draw: function () {
 		var length = this.allX.length;
+		context.beginPath();
 		for (var i = 0; i < length; i++) {
 			this.strokeLine( this.allX[i], this.allY[i] );
 		}
@@ -29,10 +26,9 @@ var Freehand = Shape.extend({
 
 	strokeLine: function ( x, y ) {
 		context.strokeStyle = this.myColor;
-    	context.beginPath();
     	context.lineTo( x, y );
     	context.stroke();
-    	context.closePath();
+    	
 	}
 });
 console.log( "Freehand class loaded." );
